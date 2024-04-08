5ae975e92654690ab0b4e4e14fc0ce41b8c47598
Azize <azizedirilik6@gmail.com>
2024-04-08 23:48:51 +0300

    stack demo

---
+++ lib/demos/stack_demo_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_101/core/random_image.dart';
+
+class StackDemoView extends StatelessWidget {
+  const StackDemoView({super.key});
+  final _cardHeight = 50.0;
+  final _cardWidth = 100.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 4,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned.fill(
+                      bottom: _cardHeight / 2, child: const RandomImage()),
+                  Positioned(
+                      height: _cardHeight,
+                      bottom: 0,
+                      width: _cardWidth,
+                      child: _cardCustom())
+                ],
+              )),
+          const Spacer(
+            flex: 6,
+          )
+        ],
+      ),
+    );
+  }
+
+  Card _cardCustom() {
+    return const Card(
+      color: Colors.white,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_101/101/stack_learn.dart';
+import 'package:flutter_101/demos/stack_demo_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
             centerTitle: true,
             elevation: 0,
           )),
-      home: const StacLearn(),
+      home: const StackDemoView(),
     );
   }
 }
+
